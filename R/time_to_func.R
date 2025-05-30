66c51af71f566743951528d87a86cfaa0e02c937
Vedant Kansara <vedant@MacBookPro.lan>
2025-05-30 10:53:13 -0400

    New time to command score function

--- R/time_to_func.R
+++ R/time_to_func.R
@@ -3,14 +3,14 @@ time_to_crsr <- function(data, database){
 
   # Get the raw CRSR data using the get CRSR func
   raw_crsr_df <- crsr_func(data = data, database = database)
-  
+
   # Get the gose data for wlst and discharge_gose data
   gose_df <- gose_func(data = data)
-  
+
   # Get diagnosis date and icu discahrge date
   icu_date_df <- demo_func(data = data, database = database) %>%
     select(record_id, primary_adm_dx_onset, icu_dch_date)
-  
+
   crsr_recov_min_dt <- raw_crsr_df %>%
     select(record_id, date, crsr_total, cs_group) %>%
     group_by(record_id) %>%
@@ -19,10 +19,10 @@ time_to_crsr <- function(data, database){
     ungroup() %>%
     select(-crsr_total, -cs_group) %>%
     rename(min_date = date)
-  
+
   crsr_wlst_df <- gose_df %>%
     select(record_id, wlst, discharge_gose)
-  
+
   time_crsr_df <- raw_crsr_df %>%
     select(-time, -cs_group) %>%
     left_join(crsr_wlst_df, by = "record_id") %>%
@@ -43,8 +43,60 @@ time_to_crsr <- function(data, database){
     select(-date, -crsr_total) %>%
     distinct() %>%
     select(record_id, crsr_recov, crsr_status, crsr_time_to)
-  
+
   return(time_crsr_df)
+}
+
+################################################################################
+################################################################################
+################################################################################
+
+### Time to Command score function
+time_to_command_foll <- function(data, database){
+
+  # Get the raw CRSR data using the get CRSR func
+  raw_command_df <- command_score_func(data = data, database = database)
+
+  # Get the gose data for wlst and discharge_gose data
+  gose_df <- gose_func(data = data)
+
+  # Get diagnosis date and icu discahrge date
+  icu_date_df <- demo_func(data = data, database = database) %>%
+    select(record_id, primary_adm_dx_onset, icu_dch_date)
+
+  command_recov_min_dt <- raw_command_df %>%
+    select(record_id, date, command_score) %>%
+    group_by(record_id) %>%
+    filter(command_score >= 4 ) %>%
+    slice_min(date, with_ties = FALSE) %>%
+    ungroup() %>%
+    rename(min_date = date)
+
+  command_wlst_df <- gose_df %>%
+    select(record_id, wlst, discharge_gose)
+
+  time_command_df <- raw_command_df %>%
+    select(-time) %>%
+    left_join(command_wlst_df, by = "record_id") %>%
+    left_join(command_recov_min_dt, by = "record_id") %>%
+    left_join(icu_date_df, by = "record_id") %>%
+    group_by(record_id) %>%
+    filter(date >= primary_adm_dx_onset) %>%
+    filter(date <= icu_dch_date) %>%
+    mutate(command_recov = ifelse(any(command_score >= 4), "yes", "no")) %>%
+    mutate(command_status = case_when(command_recov == "yes" ~ 1,
+                                      command_recov == "no" & !discharge_gose == 1 ~ 2,
+                                      command_recov == "no" & wlst == 0 & discharge_gose == 1 ~ 0,
+                                      command_recov == "no" & wlst == 1 ~ 2),
+           command_time_to = case_when(command_status == 1 ~ as.numeric(min_date - primary_adm_dx_onset),
+                                       command_status == 0 ~ as.numeric(icu_dch_date - primary_adm_dx_onset),
+                                       command_status == 2 & wlst == 1 ~ as.numeric(icu_dch_date - primary_adm_dx_onset),
+                                       command_status == 2 & wlst == 0 ~ 31)) %>%
+    select(-date, -command_score) %>%
+    distinct() %>%
+    select(record_id, command_recov, command_status, command_time_to)
+
+  return(time_command_df)
 }
 
 ################################################################################
@@ -54,17 +106,17 @@ time_to_crsr <- function(data, database){
 #Time to MCSp function
 
 time_to_mcsp <- function(data, database){
-  
+
   # Get the raw CRSR data using the CRSR func
   raw_crsr_df <- crsr_func(data = data, database = database)
-  
+
   # Get the gose data for wlst and discharge_gose data
   gose_df <- gose_func(data = data)
-  
+
   # Get diagnosis date and icu discahrge date
   icu_date_df <- demo_func(data = data, database = database) %>%
     select(record_id, primary_adm_dx_onset, icu_dch_date)
-  
+
   mcsp_recov_min_dt <- raw_crsr_df %>%
     select(record_id, date, crsr_total, cs_group) %>%
     group_by(record_id) %>%
@@ -73,10 +125,10 @@ time_to_mcsp <- function(data, database){
     ungroup() %>%
     select(-crsr_total, -cs_group) %>%
     rename(min_date = date)
-  
+
   crsr_wlst_df <- gose_df %>%
     select(record_id, wlst, discharge_gose)
-  
+
   time_mcsp_df <- raw_crsr_df %>%
     select(-time, -crsr_total) %>%
     left_join(crsr_wlst_df, by = "record_id") %>%
@@ -97,9 +149,9 @@ time_to_mcsp <- function(data, database){
     select(-date, -cs_group) %>%
     distinct() %>%
     select(record_id, mcsp_recov, mcsp_status, mcsp_time_to)
-  
+
   return(time_mcsp_df)
-  
+
 }
 
 
@@ -110,17 +162,17 @@ time_to_mcsp <- function(data, database){
 # Time to MCSm function
 
 time_to_mcspm <- function(data, database){
-  
+
   # Get the raw CRSR data using the CRSR func
   raw_crsr_df <- crsr_func(data = data, database = database)
-  
+
   # Get the gose data for wlst and discharge_gose data
   gose_df <- gose_func(data = data)
-  
+
   # Get diagnosis date and icu discahrge date
   icu_date_df <- demo_func(data = data, database = database) %>%
     select(record_id, primary_adm_dx_onset, icu_dch_date)
-  
+
   mcspm_recov_min_dt <- raw_crsr_df %>%
     select(record_id, date, crsr_total, cs_group) %>%
     group_by(record_id) %>%
@@ -129,10 +181,10 @@ time_to_mcspm <- function(data, database){
     ungroup() %>%
     select(-crsr_total, -cs_group) %>%
     rename(min_date = date)
-  
+
   crsr_wlst_df <- gose_df %>%
     select(record_id, wlst, discharge_gose)
-  
+
   time_mcspm_df <- raw_crsr_df %>%
     select(-time, -crsr_total) %>%
     left_join(crsr_wlst_df, by = "record_id") %>%
@@ -153,9 +205,9 @@ time_to_mcspm <- function(data, database){
     select(-date, -cs_group) %>%
     distinct() %>%
     select(record_id, mcspm_recov, mcspm_status, mcspm_time_to)
-  
+
   return(time_mcspm_df)
-  
+
 }
 
 ################################################################################
@@ -165,14 +217,14 @@ time_to_mcspm <- function(data, database){
 # Time to GOS-E function
 
 time_to_gose <- function(data, database){
-  
+
   # Get the gose data for wlst and discharge_gose data
   gose_df <- gose_func(data = data)
-  
+
   # Get diagnosis date and icu discahrge date
   icu_date_df <- demo_func(data = data, database = database) %>%
     select(record_id, primary_adm_dx_onset, icu_dch_date)
-  
+
   time_gose_df <- gose_df %>%
     select(-best_gose) %>%
     left_join(icu_date_df, by = "record_id") %>%
@@ -201,9 +253,9 @@ time_to_gose <- function(data, database){
     rename(gose_recov = recovered,
            gose_status = status,
            gose_time_to = time)
-  
+
   return(time_gose_df)
-  
+
 }
 
 
